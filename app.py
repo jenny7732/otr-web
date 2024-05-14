e9daeeecc406094edaa07121dfd122646d3153da
이유진 <jenny7732@naver.com>
2024-05-14 15:26:34 +0900

    feat: 문장 post 기능추가

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from synthesize import synthesize_speech
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('main.html')
+
+@app.route('/sentence', methods=['POST'])
+def script():
+    sentence = request.form['sentence']
+    synthesize_speech(sentence)
+    return sentence
 
 
 if __name__ == '__main__':
