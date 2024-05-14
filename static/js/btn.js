6c73deb21d96f7b46fb287fefb59db792d027c36
이유진 <jenny7732@naver.com>
2024-05-14 16:39:13 +0900

    style: 텍스트 입력 받으면 버튼 색상 변경

---
+++ static/js/btn.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("input-textarea").addEventListener("input", function() {
+    var submitBtn = document.getElementById("submit-btn");
+    if (this.value.trim() !== "") {
+        submitBtn.style.backgroundColor = "#C24914";
+    } else {
+        submitBtn.style.backgroundColor = "#D9D9D9";
+    }
+});
