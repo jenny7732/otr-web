c66b201bad18b0d287f7f9dbf78bfbcb05116033
이유진 <jenny7732@naver.com>
2024-05-14 14:45:29 +0900

    style: 배경 및 이모티콘 넣기

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from synthesize import synthesize_speech
 
 app = Flask(__name__)
 
@@ -7,12 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('main.html')
-
-@app.route('/sentence', methods=['POST'])
-def script():
-    sentence = request.form['sentence']
-    synthesize_speech(sentence)
-    return sentence
 
 
 if __name__ == '__main__':
