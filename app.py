3e37f6097dc819c4d49b580da8a9e12c5c5019ab
Mini <minnnidev@gmail.com>
2024-05-14 10:22:18 +0900

    feat: 문장 등록 기능 구현

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('main.html')
+
+@app.route('/sentence', methods=['POST'])
+def script():
+    sentence = request.form['sentence']
+    return sentence
 
 
 if __name__ == '__main__':
