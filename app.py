602e1d16abf62f8e5c7b8521af90173642403a30
Mini <minnnidev@gmail.com>
2024-05-19 16:15:08 +0900

    feat: git lfs 세팅

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
-from synthesize import synthesize_speech
+from http import HTTPStatus
+import json
+from flask import Flask, render_template, request, jsonify
+from TextToSpeech.synthesizer import synthesize
 
 app = Flask(__name__)
 
@@ -8,11 +10,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template('main.html')
 
-@app.route('/sentence', methods=['POST'])
-def script():
+@app.route('/synthesize', methods=['POST'])
+def synthesize_sentence():
     sentence = request.form['sentence']
-    synthesize_speech(sentence)
-    return sentence
+    synthesize(sentence)
+    return jsonify({"status": HTTPStatus.OK,
+                    "senetence": sentence, 
+                    "message": "음성 합성 성공"})
 
 
 if __name__ == '__main__':
--- synthesize.py
+++
@@ -1,3 +0,0 @@
-
-def synthesize_speech(text):
-    print("음성 합성 실시")
