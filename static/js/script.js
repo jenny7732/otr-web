f15ac2ea4961c4e632c61e33cb4014a0b682f729
이유진 <jenny7732@naver.com>
2024-05-28 10:35:27 +0900

    feat: 사용자 음성 녹음 기능 추가

--- static/js/script.js
+++ static/js/script.js
@@ -9,102 +9,105 @@ document.getElementById("input-textarea").addEventListener("input", function() {
     }
 });
 
+
 const btn = document.getElementById("analysis-btn"); 
 const modal = document.getElementById("modalWrap"); 
 const closeBtn = document.getElementById("closeBtn"); 
 
+btn.disabled = true;
+
 btn.onclick = function () {
-  modal.style.display = "block"; 
+    
+    // analysis-btn 클릭 시 서버에 GET 요청을 보냅니다.
+    // const audioUrl = document.getElementById("audio_url").innerText;
+    
+    fetch(`/similarity`, {method: 'GET'})
+        .then(response => response.json())
+        .then(data => {
+            // 응답에서 similarity를 가져와서 HTML에 표시합니다.
+            const similarity = document.getElementById("similarity");
+            const result = document.getElementById("result");
+            similarity.textContent = data.similarity;
+            result.textContent = `당신의 올투리력은 ${data.similarity}% 입니다`;
+        })
+        .catch(error => console.error('Error:', error));
+    
+    // 모달창을 표시합니다.
+    modal.style.display = "block"; 
 };
 
 closeBtn.onclick = function () {
-  modal.style.display = "none"; 
+    modal.style.display = "none"; 
 };
 
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none"; 
-  }
+    if (event.target == modal) {
+        modal.style.display = "none"; 
+    }
 };
 
-let isRecording = false;
-let recorder = null;
-let chunks = [];
-let audioPlayer = null; 
 
-document.querySelector('.voice-btn').addEventListener('click', async () => {
-    if (!isRecording) {
-        startRecording();
-    } else {
-        stopRecording();
-    }
-});
+let audioRecorder;
+var audio = document.querySelector('audio'); 
 
-function startRecording() {
-    isRecording = true;
-    chunks = [];
-
-    if (audioPlayer) {
-        audioPlayer.remove();
-    }
+function startRecording(){
     navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(function(stream) {
-            recorder = new MediaRecorder(stream);
-            recorder.ondataavailable = function(e) {
-                chunks.push(e.data);
-            };
-            recorder.onstop = function() {
-                isRecording = false;
-                const blob = new Blob(chunks, { 'type' : 'audio/wav' });
-                const audioURL = URL.createObjectURL(blob);
-                addAudioPlayer(audioURL);
-            };
-            recorder.start();
-        })
-        .catch(function(err) {
-            console.error('Error accessing microphone:', err);
+    .then(function (stream) {
+        audioRecorder = RecordRTC(stream, {
+            type: 'audio',
+            mimeType: 'audio/wav',
+            recorderType: StereoAudioRecorder,
+            numberOfAudioChannels: 1,
+            desiredSampRate: 16000,
+            bufferSize: 16384,
         });
-}
 
-function stopRecording() {
-    if (recorder) {
-        recorder.stop();
-    }
-}
-
-document.querySelector('.save-btn').addEventListener('click', () => {
-
-    if (chunks.length === 0) {
-        console.error('No audio data to upload');
-        return; 
-    }
-
-    const blob = new Blob(chunks, { 'type' : 'audio/wav' });
-    const formData = new FormData();
-    formData.append('audio', blob, 'recorded_audio.wav');
-
-    fetch('/upload', {
-        method: 'POST',
-        body: formData
+        audioRecorder.startRecording();
     })
-    .then(response => {
-        if (response.ok) {
-            console.log('File uploaded successfully');
-            document.getElementById('analysis-btn').style.backgroundColor = "#C24914";
-        } else {
-            console.error('Error uploading file');
-            document.getElementById('analysis-btn').style.backgroundColor = "#D9D9D9";
-        }
-    })
-    .catch(error => {
-        console.error('Error uploading file:', error);
+    .catch(function (error) {
+        console.error('getUserMedia error:', error);
     });
-});
-
-function addAudioPlayer(audioURL) {
-    const audioContainer = document.querySelector('.practice');
-    audioPlayer = document.createElement('audio');
-    audioPlayer.controls = true;
-    audioPlayer.src = audioURL;
-    audioContainer.appendChild(audioPlayer);
 }
+
+function stopRecording(){
+    if (audioRecorder) {
+        audioRecorder.stopRecording(function() {
+            let audioBlob = audioRecorder.getBlob();
+            const url = window.URL.createObjectURL(audioBlob);
+            audio.src = url;
+            audio.play();
+            
+            // // 파일 다운로드
+            // const a = document.createElement('a');
+            // a.href = url;
+            // a.download = 'recorded_audio.wav';
+            // a.click();
+            setTimeout(() => {
+                window.URL.revokeObjectURL(url);
+            }, 100);
+
+            // 서버에 업로드
+            const formData = new FormData();
+            formData.append('audio', audioBlob, 'recorded_audio.wav');
+            fetch('/upload', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.message);
+                console.log(data.file_path);
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    } else {
+        console.error('audioRecorder is not defined');
+    }
+
+    document.getElementById("analysis-btn").style.backgroundColor = "#C24914";
+    btn.disabled = false;
+}
+
+document.getElementById('startBtn').addEventListener('click', startRecording);
+document.getElementById('stopBtn').addEventListener('click', stopRecording);
+
