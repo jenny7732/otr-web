f4ae339576ac2cfd6bd7ff44bd30541d65f3a1c1
이유진 <jenny7732@naver.com>
2024-05-28 10:48:40 +0900

    feat: 녹음음성 flask 서버 전송

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
 from http import HTTPStatus
 import json
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from TextToSpeech.synthesizer import synthesize
+from percent import calculate_similarity
+
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = '/Users/user/otr-web/static/audio/userAudio'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def hello_world():
-    return render_template('main.html')
+    audio_url = request.args.get('audio_url')
+    sentence = request.args.get('sentence')
+    return render_template('main.html', audio_url=audio_url, sentence=sentence)
 
 @app.route('/synthesize', methods=['POST'])
 def synthesize_sentence():
     sentence = request.form['sentence']
-    synthesize(sentence)
-    return jsonify({"status": HTTPStatus.OK,
-                    "senetence": sentence, 
-                    "message": "음성 합성 성공"})
+    audio_url = synthesize(sentence)
+
+    return redirect(url_for('hello_world', audio_url = audio_url, sentence=sentence))
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'audio' not in request.files:
+        return jsonify({"message": "No audio part"}), 400
+
+    file = request.files['audio']
+    if file.filename == '':
+        return jsonify({"message": "No selected file"}), 400
+
+    if file:
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return jsonify({"message": "File successfully uploaded", "file_path": file_path}), 200
+    
+@app.route('/similarity', methods=['GET'])
+def get_similarity_score():
+    audio_url = request.args.get('audio_url')
+    file_path = request.args.get('file_path')
+    
+    # audio_url과 file_path를 이용하여 유사성 점수 계산
+    similarity_score = calculate_similarity(audio_url, file_path)
+    
+    return jsonify({"similarity": similarity_score})
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
